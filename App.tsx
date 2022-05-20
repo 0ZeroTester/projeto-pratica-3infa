a3e9621aedd02e34987374872fbf0ff7ee60d1f4
0ZeroTester <zerotester360@gmail.com>
2022-05-20 19:31:09 -0300

    alteração do texto da página inicial

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Com a chegada do outono, nas cidades frias da costa, o pôr-do-sol torna-se mais belo.</Text>
       <StatusBar style="auto" />
     </View>
   );
